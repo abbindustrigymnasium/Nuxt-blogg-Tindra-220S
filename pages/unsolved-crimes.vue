646237b7b80c779f1ddaf3c3fe78f8ef54ec4608
abbtinake <tindra.akerberg@abbgymnasiet.se>
2023-05-03 20:46:51 +0200

    quick fixes

--- pages/unsolved-crimes.vue
+++ pages/unsolved-crimes.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <img class="h-80" src="~/assets/images/Madeleine McCann1.jpg">
-    <p class="text-xl font-mono">Madeleine McCann was a three-year-old British girl who disappeared on the evening of May 3, 2007, while on holiday with her family in the Portuguese resort town of Praia da Luz.
+    <p class="text-xl font-mono text-white">Madeleine McCann was a three-year-old British girl who disappeared on the evening of May 3, 2007, while on holiday with her family in the Portuguese resort town of Praia da Luz.
          Her disappearance quickly became one of the most high-profile missing persons cases in modern history,
          with extensive media coverage and international attention. Despite an extensive investigation and ongoing efforts to find her,
          Madeleine's whereabouts remain unknown. The case has sparked numerous theories and controversies, and has had a profound impact on her family and those who have followed the case over the years.</p>
